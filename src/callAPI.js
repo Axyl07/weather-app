f2e92e1c067fd93e50df3a725847bbf8f0b7a240
Axyl07 <aryanchauhan000777@gmail.com>
2025-01-02 21:46:54 +0530

    Fetching data with input value done

--- src/callAPI.js
+++ src/callAPI.js
@@ -2,12 +2,19 @@
 export default async function callAPI(location) {
   const URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/today?unitGroup=metric&key=T2UVVWT53KJE8WTBH6P4XVRRE&contentType=json`;
   const response = await fetch(URL);
-  const json = await response.json()
-  console.log("whole data"+json);
-  console.log("current temp"+json.days[0].temp);
-  console.log("max temp"+json.days[0].tempmax);
-  console.log("min temp"+json.days[0].tempmin);
-  console.log("windspeed"+json.days[0].windspeed);
-  console.log("description"+json.description);
-  return json;
+  const responseInJSON = await response.json()
+  const dataObject = {
+    currenttemp: responseInJSON.days[0].temp,
+    hightemp: responseInJSON.days[0].tempmax,
+    lowtemp: responseInJSON.days[0].tempmin,
+    wind: responseInJSON.days[0].windspeed,
+    description: responseInJSON.description
+}
+  console.log(`whole data for ${location}`+dataObject);
+  console.log("current temp "+dataObject.currenttemp);
+  console.log("max temp "+dataObject.hightemp);
+  console.log("min temp "+dataObject.lowtemp);
+  console.log("windspeed "+dataObject.wind);
+  console.log("description "+dataObject.description);
+  return dataObject;
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import callAPI from './callAPI'
-import './index.css';
-import formControl from './formControl';
+import callAPI from './callAPI';
+import "./index.css";
+// callAPI('london');
 
-callAPI('delhi');
-formControl();
-// console.log('test')
+
+
+// import processJSON from "./processJSON";
+// console.log(processJSON('delhi'));
+  // const form = document.querySelector(form);
+  const goButton = document.querySelector("#go");
+  goButton.addEventListener("click", (event) => {
+    const locationInputValue = document.querySelector("#locationInput").value;
+    console.log("testin");
+    console.log(locationInputValue);
+    console.log(`for ${locationInputValue} data is`+callAPI(locationInputValue));
+    event.preventDefault();
+  });
+
 
