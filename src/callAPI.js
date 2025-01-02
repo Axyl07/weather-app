d75d16e3223f16205c4d1014445f3014af28ce6e
Axyl07 <aryanchauhan000777@gmail.com>
2025-01-02 23:00:48 +0530

    Add data contents to UI

--- src/callAPI.js
+++ src/callAPI.js
@@ -3,18 +3,5 @@ export default async function callAPI(location) {
   const URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/today?unitGroup=metric&key=T2UVVWT53KJE8WTBH6P4XVRRE&contentType=json`;
   const response = await fetch(URL);
   const responseInJSON = await response.json()
-  const dataObject = {
-    currenttemp: responseInJSON.days[0].temp,
-    hightemp: responseInJSON.days[0].tempmax,
-    lowtemp: responseInJSON.days[0].tempmin,
-    wind: responseInJSON.days[0].windspeed,
-    description: responseInJSON.days[0].description
-}
-  console.log(responseInJSON);
-  console.log("current temp "+dataObject.currenttemp);
-  console.log("max temp "+dataObject.hightemp);
-  console.log("min temp "+dataObject.lowtemp);
-  console.log("windspeed "+dataObject.wind);
-  console.log("description "+dataObject.description);
-  return dataObject;
+  return responseInJSON;
 }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,39 @@
-import callAPI from './callAPI';
+
+import callAPI from "./callAPI";
+
 import "./index.css";
-// callAPI('london');
 
 
-
-// import processJSON from "./processJSON";
-// console.log(processJSON('delhi'));
-  // const form = document.querySelector(form);
-  const goButton = document.querySelector("#go");
-  goButton.addEventListener("click", (event) => {
-    const locationInputValue = document.querySelector("#locationInput").value;
-    console.log("testin");
-    console.log(locationInputValue);
-    console.log(`for ${locationInputValue} data is`+callAPI(locationInputValue));
-    event.preventDefault();
-  });
-
-
+const goButton = document.querySelector("#go");
+goButton.addEventListener("click", (event) => {
+  const locationInputValue = document.querySelector("#locationInput").value;
+  callAPI(locationInputValue).then(dataObject => {
+    const createdDataObject = {
+            currenttemp: dataObject.days[0].temp,
+            hightemp: dataObject.days[0].tempmax,
+            lowtemp: dataObject.days[0].tempmin,
+            wind: dataObject.days[0].windspeed,
+            description: dataObject.days[0].description,
+          };
+    console.log(createdDataObject);
+    console.log("current temp " + createdDataObject.currenttemp);
+    console.log("max temp " + createdDataObject.hightemp);
+    console.log("min temp " + createdDataObject.lowtemp);
+    console.log("windspeed " + createdDataObject.wind);
+    console.log("description " + createdDataObject.description);
+    console.log(`for ${locationInputValue}:`);
+    const currentTempDiv = document.querySelector(".currentTempDiv");
+    const maxTempDiv = document.querySelector(".maxTempDiv");
+    const minTempDiv = document.querySelector(".minTempDiv");
+    const windSpeed = document.querySelector(".windSpeed");
+    const description = document.querySelector(".description");
+  
+    // FIll UI
+    currentTempDiv.textContent = createdDataObject.currenttemp;
+    maxTempDiv.textContent = createdDataObject.hightemp;
+    minTempDiv.textContent = createdDataObject.lowtemp;
+    windSpeed.textContent = createdDataObject.wind;
+    description.textContent = createdDataObject.description;
+   });
+  event.preventDefault();
+});
